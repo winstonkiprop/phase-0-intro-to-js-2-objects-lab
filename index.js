7ab0f800648403f898ccf0cfdcb834c4323ec733
winston kiprop <winstonekip50@gmail.com>
2022-06-04 17:59:14 +0300

    commit tree 2b2fa54711b2ce574d0d355146077d7d57913b99

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name:"kiprop",
+    streetAddress: "12 broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+     newEmployee[key]=value;
+     return newEmployee;
+}function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
